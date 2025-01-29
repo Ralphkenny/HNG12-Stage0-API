40fd06e04652b7e0d03bfc34c024f17b2d1c1ef9
USER <USER@KENNY>
2025-01-29 13:33:17 +0000

    Add project files.

---
+++ Controllers/InfoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.WebRequestMethods;
+
+namespace HNGApi__0_.Controllers
+{
+    [Route("api/info")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetInfo()
+        {
+            var response = new
+            {
+                email = "Kennedyokpala12@gmail.com",
+                current_datetime = DateTime.UtcNow.ToString("o"),
+                github_url = "https://github.com/Ralphkenny"
+
+            };
+
+            return Ok(response);
+        }
+    }
+}
