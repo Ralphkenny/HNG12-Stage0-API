8ea3288037bf70cad61df7996adb3e0054800d06
Kennedy O <kennedyokpala12@gmail.com>
2025-01-31 06:30:13 +0000

    positioning

--- Program.cs
+++ Program.cs
@@ -3,10 +3,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+// CORS policy definition
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 builder.Services.AddCors(options =>
@@ -20,7 +23,9 @@ builder.Services.AddCors(options =>
                       });
 });
 
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -31,11 +36,10 @@ if (app.Environment.IsDevelopment())
 
 
 // Use CORS
-app.UseCors("MyAllowSpecificOrigins");
-app.UseRouting();
-
-
 app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors(MyAllowSpecificOrigins);
+
 
 app.UseAuthorization();
 
